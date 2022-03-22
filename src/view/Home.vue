cdfde5705e1416a3f59b1276c53533b3ab4bd5b8
zhoulin <2438955217@qq.com>
2022-03-22 20:45:30 +0800

    抽对象

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height: 100%;display: flex;flex-direction: column;">
+  <div style="height: 100%;display: flex;flex-direction: column;" class="page">
     <div class="bar">
       <div class="bar-item" @click="changeTab(1)">
         <div>我的</div>
@@ -31,7 +31,20 @@
       </div>
       <div></div>
     </div>
-    <div v-if="tabIndex == 2"></div>
+    <div v-if="tabIndex == 2" class="tab-box2">
+      <div class="tuodan-bar"></div>
+      <div class="chou-box">
+        <div class="chou-box-item">
+          <div class="nan-bg"></div>
+          <div class="btn-box">
+            <div class="btn">存</div>
+            <div class="btn">取</div>
+          </div>
+          <div class="btn10">十连抽</div>
+        </div>
+        <div class="chou-box-item"></div>
+      </div>
+    </div>
     <div v-if="tabIndex == 3"><div class="baitanBox"></div></div>
   </div>
 </template>
@@ -39,7 +52,7 @@
 export default {
   data() {
     return {
-      tabIndex: 1,
+      tabIndex: 2,
       myIndex: 1
     };
   },
@@ -54,6 +67,48 @@ export default {
 };
 </script>
 <style>
+.btn-box {
+  display: flex;
+}
+.btn {
+  height: 22px;
+  background: #a7d5f1;
+  width: 40px;
+  border-radius: 5px;
+}
+.nan-bg {
+  background-image: url("../assets/img/nan.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  border-radius: 10px;
+}
+.nv-bg {
+  background-image: url("../assets/img/nv.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.tab-box2 {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+.chou-box {
+  flex: 1;
+  display: flex;
+}
+.chou-box-item {
+  background: #aaa;
+  flex: 1;
+  height: 350px;
+  margin: 10px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .my-box-item {
   flex: 1;
   line-height: 40px;
@@ -94,7 +149,13 @@ export default {
   height: 3px;
   border-radius: 1px;
 }
-.bar-action {
+.tuodan-bar {
+  height: 160px;
+  background-image: url("../assets/img/tuodan.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin: 0 10px;
+  border-radius: 10px;
 }
 .baitanBox {
   width: 100vw;
@@ -102,5 +163,10 @@ export default {
   background-image: url("../assets/img/home.jpg");
   background-size: cover;
   background-repeat: no-repeat;
+}
+.page {
+  background-image: url("../assets/img/bg.png");
+  /* background-size: cover; */
+  background-repeat: repeat;
 }
 </style>
