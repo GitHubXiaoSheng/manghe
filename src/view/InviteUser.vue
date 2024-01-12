1886dc01f239c58a7f5801e17b919ad1a8e3f4d4
zhoulin <2438955217@qq.com>
2024-01-12 15:42:57 +0800

    用户

---
+++ src/view/InviteUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    var inviter = this.$router.id;
+    post("api/userCard/InviteUser", { inviter: inviter }).then(res => {
+        
+    });
+    this.$route.push("home");
+  }
+};
+</script>
